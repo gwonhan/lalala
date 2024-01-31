733edca3eb44207cdaf7a7550eac2d336f067998
gwonhan <h2odream21@gmail.com>
2024-01-31 14:30:52 +0900

    [Test] 프로그램 test

---
+++ src/main/java/com/ohgiraffers/test/TestAppltcation.java
@@ -0,0 +1,25 @@
+package com.ohgiraffers.test;
+
+import com.sun.tools.javac.Main;
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+
+public class TestAppltcation {
+
+    public static void main(String[] args) {
+        System.out.println("바라라라랄라라.");
+    }
+
+    }
+// 필기
+// 여기는 주석입니다. /* 는 여러줄 할 때 */
+/* JDK는 자바 컨파일러로(변역) 클래스로 변경해줌
+ * ctrl - d  는 복사 안되면 ctrl - y
+ * ctrl - space  는 자동완성 기능
+* -Dfile.encoding=UTF-8 안깨지게 하기
+-Dconsole.encoding=UTF-8 위와 동일
+* 그래도 안되면 file-invalidate caches 에서 모두 선택 후 재부팅
+대괄호,중괄호,슬래시 열고 닫기 중요 안그러면 오류  */
+
+
